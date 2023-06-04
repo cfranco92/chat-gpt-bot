e175c982d20764b7219392c13f6de7d96399f514
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-03 21:45:10 -0500

    CI-CD: Finished first SSE connection

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,11 +1,18 @@
-import { ChatGPTMessage } from "@/lib/openai-stream";
+import {
+  ChatGPTMessage,
+  OpenAIStream,
+  OpenAIStreamPayload,
+} from "@/lib/openai-stream";
+
 import { MessageArraySchema } from "@/lib/validators/message";
 import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt";
 
 export async function POST(req: Request, res: Response) {
-  const { message } = await req.json();
+  const { messages } = await req.json();
 
-  const parsedMessages = MessageArraySchema.parse(message);
+  console.log(messages);
+
+  const parsedMessages = MessageArraySchema.parse(messages);
 
   const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
     role: message.isUserMessage ? "user" : "system",
@@ -16,4 +23,20 @@ export async function POST(req: Request, res: Response) {
     role: "system",
     content: chatbotPrompt,
   });
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
